761a9097da546fe49f0d6dc872c1bb1842d4546d
mtnshr <mtnshr@gmail.com>
2025-07-15 10:51:01 +0300

    Time: 5 ms (5.33%), Space: 57.1 MB (90.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,12 +1,34 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] new_nums = new int[nums.length];
-        k = k % nums.length;
-        for (int i = 0; i <= nums.length-1; i++) {
-            new_nums[(i+k)%nums.length] = nums[i];
+        int n = nums.length;
+    
+        //Edge cases where nums contains 0 or 1 integers or k > n //
+        if (n == 0 || n == 1 || k%n == 0) {
+            //Do nothing//
         }
-        for(int i = 0; i <= nums.length-1; i++) {
-            nums[i] = new_nums[i];
+
+        //Adjusting k to be smaller then n//
+        else if (n > 0) {
+            k = k%n;
+    
+            for(int i = 0; i < n/2; i++ ) {
+                int temp;
+                temp = nums[i];
+                nums[i] = nums[n-1-i];
+                nums[n-1-i] = temp;  
+            }
+            for(int i = 0; i < k/2 ; i++) {
+                int temp;
+                temp = nums[i];
+                nums[i] = nums[k-1-i];
+                nums[k-1-i] = temp; 
+            }
+            for (int i = 0; i <= (n-k-1)/2 ; i++) {
+                int temp;
+                temp = nums[k+i];
+                nums[k+i] = nums[n-1-i];
+                nums[n-1-i] = temp;
+            }
         }
     }
 }
