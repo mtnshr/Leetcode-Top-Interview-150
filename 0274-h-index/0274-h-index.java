900571834bb4976dad565528b35bc52615a5f558
mtnshr <mtnshr@gmail.com>
2025-07-29 10:05:40 +0300

    Time: 17 ms (5.58%), Space: 41.3 MB (92.12%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int HIndex = 0;
+        int n = citations.length;
+        if (n == 1) {
+            if (citations[0] == 0) {
+                HIndex = 0;
+            } else {
+                HIndex = 1;
+            }
+        } else {
+            for (int i = 0; i <= n; i++) {
+                int counter = 0;
+                for (int j = 0; j < n; j++) {
+                    if (citations[j] >= i) {
+                        counter++;
+                    }
+                }
+                if (counter >= i && i > HIndex) {
+                    HIndex = i;
+                }
+            }
+        }
+        return HIndex;
+    } 
+}
