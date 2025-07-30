b9787a5aa5b290e26557e5ff8f802c703e461068
mtnshr <mtnshr@gmail.com>
2025-07-30 10:15:50 +0300

    Time: 0 ms (100%), Space: 41.5 MB (82.12%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,17 +1,31 @@
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int n = citations.length;
-        int hIndex = 0;
+        int[] counts = new int[citations.length+1]; //Create an empty array in the size of citations
+        int HIndex = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            int hCandidate = n - i;
-            if (citations[i] >= hCandidate) {
-                hIndex = hCandidate; // update to the higher valid h
+        //Fill the empty array with 0 values.
+        for (int i = 0; i <= citations.length; i++) {
+            counts[i]= 0;
+        }
+
+        //Fill the array counts with each citation's instances frequency
+        for (int i = 0; i <= citations.length-1; i++) {
+            if (citations[i] < citations.length) {
+                counts[citations[i]]++;
+            } else { 
+                counts[citations.length]++;
             }
         }
-        return hIndex;
-    }
+        int sum = 0;
+
+        // Check what's the maximum H index.
+        for (int i = citations.length; i >= 0 ; i--) {
+            sum = sum + counts[i];
+            if (sum >= i) {
+                HIndex = i;
+                i = -1;
+            }
+        }
+        return HIndex;
+    } 
 }
