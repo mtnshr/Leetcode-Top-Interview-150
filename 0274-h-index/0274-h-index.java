20d582cf8962a391ced559d52f2ff4b5895e890b
mtnshr <mtnshr@gmail.com>
2025-07-30 10:03:50 +0300

    Time: 0 ms (100%), Space: 41.7 MB (53.28%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,26 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        int[] counts = new int[n+1]; //Create an empty array in the size of citations
+        int[] counts = new int[citations.length+1]; //Create an empty array in the size of citations
         int HIndex = 0;
 
         //Fill the empty array with 0 values.
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= citations.length; i++) {
             counts[i]= 0;
         }
 
         //Fill the array counts with each citation's instances frequency
-        for (int i = 0; i <= n-1; i++) {
-            if (citations[i] < n) {
+        for (int i = 0; i <= citations.length-1; i++) {
+            if (citations[i] < citations.length) {
                 counts[citations[i]]++;
             } else { 
-                counts[n]++;
+                counts[citations.length]++;
             }
         }
         int sum = 0;
 
         // Check what's the maximum H index.
-        for (int i = n; i >= 0 ; i--) {
+        for (int i = citations.length; i >= 0 ; i--) {
             sum = sum + counts[i];
             if (sum >= i) {
                 HIndex = i;
